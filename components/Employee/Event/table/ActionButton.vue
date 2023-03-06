385a27cd80383e3dad26ee2142400fe20e5ed736
malolebrin <malolebrin@gmail.com>
2023-03-06 11:34:46 +0100

    ✨ Add actions on event item in employee detail event list

---
+++ components/Employee/Event/table/ActionButton.vue
@@ -0,0 +1,99 @@
+<template>
+<Menu
+  as="div"
+  class="relative inline-block text-left"
+>
+  <div>
+    <MenuButton
+      :data-cy="`event-${props.event.id}-options-links`"
+      class="inline-flex justify-center w-full px-2 py-1 text-sm font-medium text-purple-500 bg-purple-300 border border-purple-500 rounded-md bg-opacity-20 hover:bg-opacity-40 hover:text-purple-800 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+    >
+      Options
+      <ChevronDownIconSolid
+        class="w-5 h-5 ml-1 -mr-1 text-purple-500 hover:text-purple-800"
+        aria-hidden="true"
+      />
+    </MenuButton>
+  </div>
+
+  <transition
+    enter-active-class="transition duration-100 ease-out"
+    enter-from-class="transform scale-95 opacity-0"
+    enter-to-class="transform scale-100 opacity-100"
+    leave-active-class="transition duration-200 ease-in"
+    leave-from-class="transform scale-100 opacity-100"
+    leave-to-class="transform scale-95 opacity-0"
+  >
+    <MenuItems
+      class="absolute right-0 z-10 w-56 mt-2 overflow-visible origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+    >
+      <div class="px-1 py-1">
+        <MenuItem>
+          <NuxtLink
+            class="flex items-center w-full px-2 py-2 text-sm rounded-md group"
+            :to="{ name: 'evenement-show-id', params: { id: event.id } }"
+            :data-cy="`event-${event.id}-show-link`"
+          >
+            <PencilSquareIconOutline
+              class="w-5 h-5 mr-2 text-gray-800"
+              aria-hidden="true"
+            />
+            Voir
+          </NuxtLink>
+        </MenuItem>
+        <MenuItem>
+          <NuxtLink
+            class="flex items-center w-full px-2 py-2 text-sm rounded-md group"
+            :to="{ name: 'evenement-show-id', params: { id: event.id } }"
+            :data-cy="`event-${event.id}-show-link`"
+          >
+            <ArrowDownTrayIconOutline
+              class="w-5 h-5 mr-2 text-gray-800"
+              aria-hidden="true"
+            />
+            Télécharger
+          </NuxtLink>
+        </MenuItem>
+      </div>
+    </MenuItems>
+  </transition>
+</Menu>
+</template>
+
+<script setup lang="ts">
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+import type { EventType } from '@/types'
+import { ModalModeEnum, ModalNameEnum } from '@/types'
+import { useUiStore } from '~~/store'
+
+interface Props {
+  event: EventType
+}
+
+const props = defineProps<Props>()
+
+const uiStore = useUiStore()
+const { setUiModal } = uiStore
+
+// function addEmployeeToEvent() {
+//   setUiModal({
+//     isActive: true,
+//     modalName: ModalNameEnum.ADD_EMPLOYEE,
+//     modalMode: ModalModeEnum.CREATE,
+//     data: {
+//       eventId: props.event.id,
+//     },
+//   })
+// }
+
+function deleteEvent() {
+  setUiModal({
+    isActive: true,
+    modalName: ModalNameEnum.EVENT_FORM,
+    modalMode: ModalModeEnum.DELETE,
+    data: {
+      event: props.event,
+    },
+  })
+}
+</script>
--- components/Employee/Event/table/Item.vue
+++ components/Employee/Event/table/Item.vue
@@ -10,17 +10,7 @@
     <EventStatusTag :status="event.status" />
   </td>
   <td class="relative py-4 pl-3 pr-4 text-sm font-medium text-right whitespace-nowrap sm:pr-6">
-    <NuxtLink
-      class="flex items-center w-full px-2 py-2 text-sm rounded-md group"
-      :to="{ name: 'evenement-show-id', params: { id: event.id } }"
-      :data-cy="`event-${event.id}-show-link`"
-    >
-      <PencilSquareIconOutline
-        class="w-5 h-5 mr-2 text-violet-400"
-        aria-hidden="true"
-      />
-      Voir
-    </NuxtLink>
+    <EmployeeEventTableActionButton :event="event" />
   </td>
 </tr>
 </template>
