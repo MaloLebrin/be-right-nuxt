3b22f3ab141d4ffb87b8baf6bcbb5f34f556375b
malolebrin <malolebrin@gmail.com>
2023-03-20 18:58:07 +0100

    ✨ Add specific layout for account view

---
+++ layouts/account.vue
@@ -0,0 +1,21 @@
+<template>
+<main class="flex min-h-screen bg-white dark:bg-blue-dark">
+  <MenuDrawer />
+
+  <div class="w-full lg:ml-64">
+    <HeaderDashboard />
+    <PageAuthWrapper>
+      <div class="pb-10 mx-auto max-w-7xl lg:py-6 lg:px-8">
+        <div class="lg:grid lg:grid-cols-12 lg:gap-x-5">
+          <AccountMenu />
+
+          <slot />
+        </div>
+      </div>
+    </PageAuthWrapper>
+  </div>
+</main>
+</template>
+
+<script setup lang="ts">
+</script>
