5100e51258cd62ace855518fa33cd4003237a3b3
malolebrin <malolebrin@gmail.com>
2023-04-18 10:34:00 +0200

    🔥 Remove unused imports

--- types/auth.ts
+++ types/auth.ts
@@ -1,6 +1,3 @@
-import type { RoleEnum } from './Roles'
-import type { SubscriptionEnum } from '@/store/subscription/types'
-
 export interface ResetPasswordData {
   isSuccess: boolean
   message: string
