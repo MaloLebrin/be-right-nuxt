4397472db8e9da8e8123081b5d59f717df4e77d2
malolebrin <malolebrin@gmail.com>
2023-04-20 10:02:42 +0200

    ⚡️ Better SEO optimisation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,21 @@ import { type PluginOption } from 'vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: { pageTransition: { name: 'page', mode: 'out-in' } },
+  app: {
+    pageTransition: {
+      name: 'page',
+      mode: 'out-in',
+    },
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    },
+    layoutTransition: {
+      mode: 'out-in',
+      name: 'layout',
+    },
+  },
+
   typescript: {
     strict: true,
   },
