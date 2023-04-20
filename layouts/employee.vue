5884441646eefe9737d7e2c6cafee92104d57939
malolebrin <malolebrin@gmail.com>
2023-04-20 18:53:25 +0200

    ✨ Init employee layout and create error loading answer

---
+++ layouts/employee.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <LayoutsHeader />
+  <main class="h-full bg-white">
+    <slot />
+  </main>
+  <LayoutsFooter />
+</div>
+</template>
+
+<script setup lang="ts">
+import { useAuthStore } from '~/store'
+
+const authStore = useAuthStore()
+const route = useRoute()
+
+const { query: { email, token } } = route
+if (!authStore.getIsLoggedIn && email && token) {
+  console.log(email, '<==== email')
+}
+</script>
