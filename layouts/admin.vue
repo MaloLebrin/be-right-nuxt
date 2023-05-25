a9c1844ad5780d5a8a956ea2aff00390433a2e5d
malolebrin <malolebrin@gmail.com>
2023-05-25 17:31:36 +0200

    ✨ Ini auth layouts

---
+++ layouts/admin.vue
@@ -0,0 +1,15 @@
+<template>
+<main class="flex min-h-screen bg-white dark:bg-blue-dark">
+  <MenuDrawer />
+
+  <div class="w-full lg:ml-64">
+    <HeaderDashboard />
+    <slot />
+  </div>
+</main>
+</template>
+
+<script setup lang="ts">
+import MenuDrawer from '~/components/Menu/MenuDrawer.vue'
+import HeaderDashboard from '~/components/Header/HeaderDashboard.vue'
+</script>
---
+++ pages/admin/index.vue
@@ -0,0 +1,27 @@
+<template>
+<PageAuthWrapper>
+  Test
+</PageAuthWrapper>
+</template>
+
+<script setup lang="ts">
+import PageAuthWrapper from '~/components/Page/PageAuthWrapper.vue'
+import { useUiStore } from '~/store'
+
+const { $api } = useNuxtApp()
+const uiStore = useUiStore()
+const { IncLoading, DecLoading } = uiStore
+const state = reactive({})
+
+onMounted(async () => {
+  IncLoading()
+  DecLoading()
+})
+
+definePageMeta({
+  layout: 'auth',
+  isAuth: true,
+  isAdmin: true,
+  middleware: ['admin-guards-middleware'],
+})
+</script>
