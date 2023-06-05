6ee126f4662d95197ff8408e5fd40d49c5c3e63f
malolebrin <malolebrin@gmail.com>
2023-06-05 17:28:11 +0200

    ♻️ Move admin routes in admin folder

---
+++ pages/admin/events/index.vue
@@ -0,0 +1,11 @@
+<template>
+<PageAuthWrapper>
+  <div>
+    test
+  </div>
+</PageAuthWrapper>
+</template>
+
+<script setup lang="ts">
+import PageAuthWrapper from '~/components/Page/PageAuthWrapper.vue'
+</script>
