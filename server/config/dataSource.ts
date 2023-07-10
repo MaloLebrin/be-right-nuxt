01ad8b7c7359be119d32a724c2e008e224e69ca6
malolebrin <malolebrin@gmail.com>
2023-07-10 08:35:28 +0200

    🧪 Experiment fullstack nuxt api with typeorm

---
+++ server/config/dataSource.ts
@@ -0,0 +1,32 @@
+import type { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions'
+import { DataSource } from 'typeorm'
+import { dataBaseConfig } from './ormconfig'
+
+export function createAppSource() {
+  const NODE_ENV = process.env.NODE_ENV
+  let connectionsOptions: PostgresConnectionOptions
+
+  if (NODE_ENV === 'production') {
+    connectionsOptions = {
+      ...dataBaseConfig.production as unknown as PostgresConnectionOptions,
+    }
+  } else if (NODE_ENV === 'test') {
+    connectionsOptions = {
+      ...dataBaseConfig.test as unknown as PostgresConnectionOptions,
+    }
+  } else {
+    connectionsOptions = {
+      ...dataBaseConfig.dev as unknown as PostgresConnectionOptions,
+      name: 'default',
+    }
+  }
+
+  return new DataSource({
+    ...connectionsOptions,
+  })
+}
+
+const AppSource = createAppSource()
+AppSource.initialize()
+
+export default AppSource
---
+++ server/config/ormconfig.ts
@@ -0,0 +1,80 @@
+// import { entities } from './src/entity'
+
+export const dataBaseConfig = {
+  production: {
+    name: 'production',
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    synchronize: true,
+    logging: false,
+    ssl: true,
+    entities: [
+      // ...entities,
+    ],
+    migrations: [
+      './src/migrations/**/*.js',
+    ],
+    subscribers: [
+      './src/subscriber/**/*.js',
+    ],
+    cli: {
+      entitiesDir: './src/entity',
+      migrationsDir: './src/migrations',
+      subscribersDir: './src/subscriber',
+    },
+  },
+
+  dev: {
+    name: 'dev',
+    type: 'postgres',
+    host: 'postgres',
+    port: 5432,
+    username: 'test',
+    password: 'test',
+    database: 'be-right-db',
+    synchronize: true,
+    logging: false,
+    entities: [
+      // `${__dirname}/../**/*.entity{.ts,.js}`,
+      // ...entities,
+    ],
+    migrations: [
+      './src/migrations/**/*.js',
+    ],
+    subscribers: [
+      './src/subscriber/**/*.js',
+    ],
+    cli: {
+      entitiesDir: './src/entity',
+      migrationsDir: './src/migrations',
+      subscribersDir: './src/subscriber',
+    },
+  },
+  test: {
+    name: 'test',
+    type: 'postgres',
+    host: process.env.HOSTNAME_TEST,
+    port: process.env.POSTGRES_PORT_TEST,
+    username: process.env.DB_USERNAME_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    url: process.env.DATABASE_URL_TEST,
+    database: 'be-right-backend-db',
+    synchronize: true,
+    logging: false,
+    entities: [
+      // `${__dirname}/../**/*.entity{.ts,.js}`,
+      // ...entities,
+    ],
+    migrations: [
+      './src/migrations/**/*.js',
+    ],
+    subscribers: [
+      './src/subscriber/**/*.js',
+    ],
+    cli: {
+      entitiesDir: './src/entity',
+      migrationsDir: './src/migrations',
+      subscribersDir: './src/subscriber',
+    },
+  },
+}
