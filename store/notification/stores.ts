1ee885198652702b0a5569f9118635f47fed8175
MaloLebrin <malolebrin@gmail.com>
2023-10-19 17:10:31 +0200

    🩹 More coherency updating notification store

--- store/notification/stores.ts
+++ store/notification/stores.ts
@@ -59,7 +59,7 @@ export const useNotificationsStore = defineStore('notification', {
 
     addMany(notifs: NotificationType[]) {
       notifs.forEach(notif => {
-        if (!this.entities.byId[notif.id]) {
+        if (!this.entities.allIds.includes(notif.id)) {
           this.entities.allIds.push(notif.id)
         }
         this.entities.byId[notif.id] = { ...notif, $isDirty: false }
