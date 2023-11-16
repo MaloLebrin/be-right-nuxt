03d709877aa52504dafa84ff2a022a7d8e871be5
Malo <malolebrin@gmail.com>
2023-11-16 16:31:03 +0100

    ⚡️ Improve Download PDF performances with BrowserLess (#338)
    * ⚡️ Improve Download PDF performances with BrowserLess
    
    * fix env var

--- composables/downloadHook.ts
+++ composables/downloadHook.ts
@@ -5,13 +5,56 @@ export function downloadHook() {
   const { $isProductionMode } = useNuxtApp()
   const authStore = useAuthStore()
 
-  const newAPI = new FetchWrapper({
-    baseUrl: $isProductionMode ? import.meta.env.VITE_API_URL as string : import.meta.env.VITE_DEV_API_URL as string,
-    token: authStore.getToken || undefined,
-    isFileRequest: true,
-  })
+  function composeFileName({
+    answerIds,
+    eventName,
+    employeeName,
+  }: {
+    answerIds: number[]
+    eventName: string
+    employeeName?: string
+  }): string {
+    if (answerIds.length > 1 || !employeeName) {
+      return `droits-images-${eventName}.pdf`
+    }
+    return `droits-images-${employeeName}.pdf`
+  }
 
-  async function downloadAnswers(ids: number[]) {
+  async function downloadAnswers({
+    answerIds,
+    eventName,
+    employeeName,
+  }: {
+    answerIds: number[]
+    eventName: string
+    employeeName?: string
+  }) {
+    if ($isProductionMode) {
+      const { data } = await useFetch<{ fileName: string; content: string }>('/api/pdf/answer', {
+        method: 'post',
+        body: {
+          token: authStore.token,
+          answerIds,
+          fileName: composeFileName({
+            answerIds,
+            eventName,
+            employeeName,
+          }),
+        },
+      })
+      return data.value
+    } else {
+      return downloadAnswerUseApi(answerIds)
+    }
+  }
+
+  async function downloadAnswerUseApi(ids: number[]) {
+    const newAPI = new FetchWrapper({
+      baseUrl: $isProductionMode ? import.meta.env.VITE_API_URL as string : import.meta.env.VITE_DEV_API_URL as string,
+      token: authStore.getToken || undefined,
+      isFileRequest: true,
+    })
+
     const { data } = await newAPI.get<{
       fileName: string
       content: string
@@ -20,6 +63,7 @@ export function downloadHook() {
 
     return data
   }
+
   return {
     downloadAnswers,
   }
