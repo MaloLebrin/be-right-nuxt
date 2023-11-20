59568fd886f669e034df4cb8740fbea40a8af2f1
MaloLebrin <malolebrin@gmail.com>
2023-11-21 00:01:46 +0100

    ✨ add days desktop and mobile for week view

---
+++ components/Calendar/WeekView/Day.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="grid grid-cols-7 text-sm leading-6 text-gray-500 sm:hidden">
+  <button
+    v-for="day in calendarStore.getCalendarData"
+    :key="day.label"
+    class="flex flex-col items-center pt-2 pb-3 capitalize"
+    :class="[getTextColor(day)]"
+    @click="setSelectedDay(day)"
+  >
+    {{ $toFormat(day.date, 'dd') }}
+    <span class="flex items-center justify-center w-8 h-8 mt-1 font-semibold">{{ $toFormat(day.date, 'DD') }}</span>
+  </button>
+</div>
+<div class="hidden grid-cols-7 -mr-px text-sm leading-6 text-gray-500 border-r border-gray-100 divide-x divide-gray-100 sm:grid">
+  <div class="col-end-1 w-14" />
+  <button
+    v-for="day in calendarStore.getCalendarData"
+    :key="day.label"
+    class="flex items-center py-3 font-semibold capitalize sm:justify-center"
+    :class="[getTextColor(day)]"
+    @click="setSelectedDay(day)"
+  >
+    <span class="not-sr-only md:sr-only">{{ $toFormat(day.date, 'dd DD') }}</span>
+    <span class="sr-only md:not-sr-only lg:sr-only">{{ $toFormat(day.date, 'ddd DD') }}</span>
+    <span class="sr-only lg:not-sr-only">{{ $toFormat(day.date, 'dddd DD') }}</span>
+  </button>
+</div>
+</template>
+
+<script setup lang="ts">
+import { type CalendarDay, useCalendarStore } from '~~/store'
+
+const calendarStore = useCalendarStore()
+const { setSelectedDay } = calendarStore
+
+const { isToday } = dateHook()
+
+function getTextColor(day: CalendarDay) {
+  if (isToday(day.date)) {
+    return 'text-indigo-500'
+  }
+  if (calendarStore.isSelectedDay(day)) {
+    return 'text-orange-500'
+  }
+  return 'text-gray-700'
+}
+</script>
