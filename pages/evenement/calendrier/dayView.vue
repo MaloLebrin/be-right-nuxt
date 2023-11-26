e555a2a3e2932b89e1b89ba93133ac0640d7efad
MaloLebrin <malolebrin@gmail.com>
2023-11-26 22:31:51 +0100

    🎉 Add day view component to calendar

---
+++ pages/evenement/calendrier/dayView.vue
@@ -0,0 +1,22 @@
+<template>
+<PageAuthWrapper>
+  Day view
+</PageAuthWrapper>
+</template>
+
+<script setup lang="ts">
+import PageAuthWrapper from '~/components/Page/PageAuthWrapper.vue'
+
+calendarHook('week')
+
+useHead({
+  title: 'Calendrier - Vue mois',
+})
+
+definePageMeta({
+  layout: 'auth',
+  isAuth: true,
+  isAdmin: false,
+  middleware: ['guards-middleware'],
+})
+</script>
