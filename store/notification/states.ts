fa3e8e0cb42891da392da8801ee6d19b2f36c9f8
Malo <malolebrin@gmail.com>
2023-11-27 09:56:12 +0100

    ✨ Add switch to display unread notifications (#352)

--- store/notification/states.ts
+++ store/notification/states.ts
@@ -4,6 +4,7 @@ import type { NotificationSubscriptionType, NotificationType } from './types'
 export function defaultNotificationsState() {
   return {
     ...createState<NotificationType>(),
+    displayOnlyUnread: false,
   }
 }
 
