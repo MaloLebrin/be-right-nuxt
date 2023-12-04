7d95563f2d4ef4f2caf2010529a668905b6b3bfd
MaloLebrin <malolebrin@gmail.com>
2023-12-04 14:25:39 +0100

    Add hover functionality to menu items

--- components/Menu/MenuBasic.vue
+++ components/Menu/MenuBasic.vue
@@ -10,6 +10,7 @@
     <li
       v-for="(item, index) in MENU_ITEMS"
       :key="index"
+      v-element-hover="(state) => onHover(state, index)"
       role="menuitem"
     >
       <template v-if="isFolder(item)">
@@ -18,24 +19,10 @@
           :is-active="isLinkActive(item)"
         />
         <ul
-          v-if="isLinkActive(item)"
+          v-if="isLinkActive(item) || isHovered === index"
           v-motion
           role="list"
           class="space-y-1"
-          :initial="{
-            y: 100,
-            opacity: 0,
-          }"
-          :enter="{
-            y: 0,
-            opacity: 1,
-            transition: {
-              type: 'spring',
-              stiffness: 250,
-              damping: 25,
-              mass: 0.5,
-            },
-          }"
         >
           <li
             v-for="(child, index) in item.children"
@@ -61,7 +48,7 @@
 </template>
 
 <script setup lang="ts">
-// import { vElementHover } from '@vueuse/components'
+import { vElementHover } from '@vueuse/components'
 import type { MenuItemContent } from '~~/types/Menu'
 import { MENU_ITEMS } from '@/helpers/menu'
 import MenuLink from '~~/components/Menu/MenuLink.vue'
@@ -72,13 +59,15 @@ function isFolder(item: MenuItemContent) {
   return item.children && item.children.length
 }
 
-// const isHovered = ref<number>(0)
+const isHovered = ref <number | null>(0)
 
-// function onHover(state: boolean, index: number) {
-//   if (state) {
-//     isHovered.value = index
-//   }
-// }
+function onHover(state: boolean, index: number) {
+  if (state) {
+    isHovered.value = index
+  } else if (isHovered.value === index) {
+    isHovered.value = null
+  }
+}
 
 function isLinkActive(item: MenuItemContent) {
   return item.linkName === route.name
