4a35714cedcbf9f5a1b667383c9719aa4efa1e6e
MaloLebrin <malolebrin@gmail.com>
2023-12-08 13:54:30 +0100

    Add event status background color and text color

--- components/Event/EventStatusTag.vue
+++ components/Event/EventStatusTag.vue
@@ -4,10 +4,10 @@
   @click="sendEvent"
 >
   <div
-    :class="`flex item-center justify-center p-2 overflow-y-auto text-xs leading-5 rounded-lg group inset-1 bg-${getStatusColor()}-50 hover:bg-${getStatusColor()}-100 border border-${getStatusColor()}-500 hover:border-${getStatusColor()}-700`"
+    :class="`flex item-center justify-center p-2 overflow-y-auto text-xs leading-5 rounded-lg group inset-1 bg-${getStatusColor(status)}-50 hover:bg-${getStatusColor(status)}-100 border border-${getStatusColor(status)}-500 hover:border-${getStatusColor(status)}-700`"
   >
     <p
-      :class="`text-${getStatusColor()}-500 group-hover:text-${getStatusColor()}-700 font-semibold uppercase`"
+      :class="`text-${getStatusColor(status)}-500 group-hover:text-${getStatusColor(status)}-700 font-semibold uppercase`"
     >
       {{ getEventStatusTranslation(status) }}
     </p>
@@ -16,34 +16,19 @@
 </template>
 
 <script setup lang="ts">
-import { EventStatusEnum } from '@/types'
+import type { EventStatusEnum } from '~~/types'
 
 interface Props {
   status: EventStatusEnum
 }
 
-const props = defineProps<Props>()
+defineProps<Props>()
 
 const emit = defineEmits<{
   (e: 'click'): void
 }>()
 
-const { getEventStatusTranslation } = eventHook()
-
-function getStatusColor() {
-  switch (props.status) {
-    case EventStatusEnum.COMPLETED:
-      return 'sky'
-    case EventStatusEnum.PENDING:
-      return 'orange'
-    case EventStatusEnum.CLOSED:
-      return 'gray'
-    case EventStatusEnum.CREATE:
-      return 'green'
-    default:
-      return 'gray'
-  }
-}
+const { getEventStatusTranslation, getStatusColor } = eventHook()
 
 function sendEvent() {
   emit('click')
