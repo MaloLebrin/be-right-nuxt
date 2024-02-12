955d2c119e6d62df11d13c8997f0101a02374b23
MaloLebrin <malolebrin@gmail.com>
2024-02-12 14:47:45 +0100

    ✨ Payment list fetched by customer Id

--- pages/mon-compte/plan-billing.vue
+++ pages/mon-compte/plan-billing.vue
@@ -212,7 +212,7 @@
   </AccountBaseCard>
 
   <AccountBaseCard title="Historique de paiement">
-    <PaymentList :payments="payments" />
+    <PaymentList />
   </AccountBaseCard>
 </div>
 </template>
@@ -234,17 +234,6 @@ const plans = [
   { name: 'Startup', priceMonthly: 29, priceYearly: 290, limit: 'Up to 5 active job postings' },
   { name: 'Business', priceMonthly: 99, priceYearly: 990, limit: 'Up to 25 active job postings' },
   { name: 'Enterprise', priceMonthly: 249, priceYearly: 2490, limit: 'Unlimited active job postings' },
-]
-const payments = [
-
-  {
-    id: 1,
-    date: '1/1/2020',
-    datetime: '2020-01-01',
-    description: 'Business Plan - Annual Billing',
-    amount: 'CA$109.00',
-    href: '#',
-  },
 ]
 
 const selectedPlan = ref(plans[1])
