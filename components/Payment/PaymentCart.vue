7f58fdb655841ddc6696b5d552d9f3aabe03da03
MaloLebrin <malolebrin@gmail.com>
2024-02-26 13:32:08 +0100

    ♻️ Add PaymentButton component and refactor PaymentCart.vue

--- components/Payment/PaymentCart.vue
+++ components/Payment/PaymentCart.vue
@@ -55,49 +55,18 @@
   </dl>
 
   <div class="mt-6">
-    <button
-      type="button"
-      :disabled="!isSubmitEnabled"
-      class="w-full px-4 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm"
-      :class="[
-        'hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50',
-        isSubmitEnabled ? '' : 'disabled:cursor-not-allowed disabled:opacity-50',
-      ]"
-      @click="checkout"
-    >
-      <div
-        v-if="uiStore.getUIIsLoading"
-        class="flex items-center justify-center w-full h-full"
-      >
-        <svg
-          class="w-6 h-6 text-white-600 animate-spin"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <circle
-            class="opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            stroke-width="4"
-          />
-          <path
-            class="opacity-75"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-          />
-        </svg>
-      </div>
-      <span v-else>Payer</span>
-    </button>
+    <PaymentButton
+      :is-loading="uiStore.getUIIsLoading"
+      :is-disabled="!isSubmitEnabled"
+      @onClick="checkout"
+    />
   </div>
 </section>
 </template>
 
 <script setup lang="ts">
 import { QuestionMarkCircleIcon } from '@heroicons/vue/24/outline'
+import PaymentButton from '~/components/Payment/PaymentButton.vue'
 import { useFormStore, useUiStore } from '~~/store'
 
 interface Props {
@@ -114,7 +83,7 @@ const emit = defineEmits(['checkout'])
 
 const uiStore = useUiStore()
 const formStore = useFormStore()
-// const { submitCreationEvent } = eventFormHook()
+const { submitCreationEvent } = eventFormHook()
 
 const isSubmitEnabled = computed(() =>
   formStore.isStepPhotographerValid
@@ -123,7 +92,7 @@ const isSubmitEnabled = computed(() =>
   && !uiStore.getUIIsLoading)
 
 async function checkout() {
-  // await submitCreationEvent()
+  await submitCreationEvent()
   emit('checkout')
 }
 </script>
