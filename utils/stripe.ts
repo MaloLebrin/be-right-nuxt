e815307d401b028f74dea98a5ba9478e6f602b37
MaloLebrin <malolebrin@gmail.com>
2024-02-28 17:04:07 +0100

    ✨ Add utility function to retrieve product

--- utils/stripe.ts
+++ utils/stripe.ts
@@ -1,12 +1,16 @@
 import type { ProductWithPrice } from '~/types'
 
 export function getUnitPrice(products: ProductWithPrice[] | null): number | undefined {
+  return getPrice(products)?.default_price?.unit_amount
+}
+
+export function getPrice(products: ProductWithPrice[] | null): ProductWithPrice | undefined {
   if (!products || products.length === 0) {
     return undefined
   }
-  return products.find(product => {
+  return products?.find(product => {
     return product.metadata?.apiType === 'UNIT'
-  })?.default_price?.unit_amount
+  })
 }
 
 export function fromCent(amount: number): number {
