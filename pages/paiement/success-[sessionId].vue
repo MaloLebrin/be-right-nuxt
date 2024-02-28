48014fc55f44f0a6afa698d7c159422658fff04a
MaloLebrin <malolebrin@gmail.com>
2024-02-28 17:02:43 +0100

    🎉 Init success and cancel Page

---
+++ pages/paiement/success-[sessionId].vue
@@ -0,0 +1,15 @@
+<template>
+<div class="h-48 bg-red-400">
+  {{ $router.currentRoute.value.params.sessionId }}
+</div>
+</template>
+
+<script setup lang="ts">
+const { $router } = useNuxtApp()
+
+definePageMeta({
+  // layout: 'auth',
+  // isAuth: true,
+  // middleware: ['guards-middleware'],
+})
+</script>
