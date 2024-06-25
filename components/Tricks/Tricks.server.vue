de2863c035e902ae89ceb11851ee8e04a7c3c13d
Malo <malolebrin@gmail.com>
2024-06-25 11:48:07 +0200

    👷🏻‍♂️ rework CI (#465)
    * 👷🏻‍♂️ rework CI
    
    * delete lock file
    
    * add lock file
    
    * update actions
    
    * fe
    
    * update pnpm version
    
    * Fix ts errors

--- components/Tricks/Tricks.server.vue
+++ components/Tricks/Tricks.server.vue
@@ -1,11 +1,11 @@
 <template>
 <div
-  v-if="data.value"
+  v-if="data"
   class="relative hidden p-2 mx-2 mt-4 text-xs bg-opacity-50 border rounded-md md:block bg-amber-200 border-amber-300"
 >
   <div class="flex justify-between mb-2">
     <p class="font-semibold max-w-3/4">
-      {{ data.value.title }}
+      {{ data.title }}
     </p>
     <button
       class="w-4 h-4 text-amber-500"
@@ -15,7 +15,7 @@
     </button>
   </div>
   <p class="font-light ">
-    {{ data.value.description }}
+    {{ data.description }}
   </p>
 </div>
 </template>
