a5d741fe5ddc4d80270279b8687d2a67b1ee3848
MaloLebrin <malolebrin@gmail.com>
2024-12-07 01:29:00 +0100

    ✨ Add cancellation page with error message and remove old cancellation component

---
+++ pages/paiement/cancel-[sessionId].vue
@@ -0,0 +1,10 @@
+<template>
+<div class="container py-6 mx-auto md:py-12">
+  <h1 class="text-center">
+    Une erreur et survenue.
+  </h1>
+</div>
+</template>
+
+<script setup lang="ts">
+</script>
--- pages/paiement/cancel.vue
+++
@@ -1,8 +0,0 @@
-<template>
-<div>
-  <h1>Le paiement a été annulé</h1>
-</div>
-</template>
-
-<script setup lang="ts">
-</script>
