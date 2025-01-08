118daf32693d913e594f6484ec9654b1d9697694
MaloLebrin <malolebrin@gmail.com>
2025-01-08 18:06:10 +0100

    feat: add blog feature with SEO metadata and update header links

---
+++ pages/blog/[...slug].vue
@@ -0,0 +1,25 @@
+<template>
+<article>
+  lrehgghr
+</article>
+</template>
+    
+<script setup lang="ts">
+useServerSeoMeta({
+  title: 'BeRight Blog - Page',
+  description: 'Gérez enfin le droit à l\'image de vos clients facilement !',
+  ogTitle: 'BeRight Blog',
+  ogDescription: 'Gérez enfin le droit à l\'image de vos clients facilement !',
+  ogUrl: 'https://be-right.co/blog',
+  ogLocale: 'fr_FR',
+  ogType: 'website',
+  articleAuthor: 'BeRight',
+  articleSection: 'Blog',
+  articleTag: 'Droit à l\'image',
+  articlePublishedTime: '2022-01-01T00:00:00Z',
+  articleModifiedTime: '2022-01-01T00:00:00Z',
+  articleExpirationTime: '2022-01-01T00:00:00Z',
+  articlePublisher: 'BeRight',
+})
+</script>
+    
