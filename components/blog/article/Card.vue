9601d95923c50c0b588b00a85d3b0a97e71444dd
MaloLebrin <malolebrin@gmail.com>
2025-01-08 22:27:09 +0100

    feat: add blog article card and list components with TypeScript types

---
+++ components/blog/article/Card.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="relative flex flex-col p-2 bg-white shadow-md rounded-3xl shadow-black/5 ring-1 ring-black/5">
+  <img
+    v-if="post.mainImage"
+    :alt="post.mainImage.alt ||
+      ''"
+    :src="image(post.mainImage).size(1170, 780).url()"
+    class="aspect-[3/2] w-full rounded-2xl object-cover">
+  <div class="flex flex-col flex-1 p-8">
+    <div class="text-gray-700 text-sm/5">
+      {{ $toFormat(post.publishedAt, 'dddd, MMMM D, YYYY') }}
+    </div>
+    <div class="mt-2 font-medium text-base/7">
+      <NuxtLink :to="`/blog/${post.slug}`">
+        <span class="absolute inset-0" />
+        {{post.title}}
+      </NuxtLink>
+    </div>
+    <div class="flex-1 mt-2 text-gray-500 text-sm/6">
+      {{post.excerpt}}
+    </div>
+    <div
+      v-if="post.author"
+      class="flex items-center gap-3 mt-6">
+      <img
+        alt=""
+        :src="image(post.author.image).size(64, 64).url()"
+        class="object-cover rounded-full aspect-square size-6">
+      <div class="text-gray-700 text-sm/5">
+        {{ post.author.name }}
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script setup lang="ts">
+import type { Post } from '~/types'
+interface CardProps {
+  post: Post
+}
+
+defineProps<CardProps>()
+</script>
---
+++ components/blog/article/ListCards.vue
@@ -0,0 +1,20 @@
+<template>
+<ul class="grid grid-cols-1 gap-8 mt-6 lg:grid-cols-3">
+  <li
+    v-for="post in posts"
+    :key="post.slug">
+    <Card :post="post" />
+  </li>
+</ul>
+</template>
+
+<script setup lang="ts">
+import Card from './Card.vue'
+import type { Post } from '~/types'
+
+interface ListCardsProps {
+  posts: Post[]
+}
+
+defineProps<ListCardsProps>()
+</script>
