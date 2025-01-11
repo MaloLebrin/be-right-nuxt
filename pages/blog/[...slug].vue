727371248aa07ba893b437fcd16c293c6981f21a
MaloLebrin <malolebrin@gmail.com>
2025-01-11 12:19:23 +0100

    refactor: update blog layout and enhance article rendering

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,32 +1,36 @@
 <template>
-<!-- <Container class-name="mt-16 pb-24">
-  <template v-if="post">
-    <ContentRenderer :value="post">
-      <h1>{{ post.title }}</h1>
-      <ContentRendererMarkdown :value="post" />
-    </ContentRenderer>
-  </template>
-  <NoArticle v-else />
+<section class="py-16 overflow-hidden">
+  <GradientBackground />
+  <Container>
+    <h2 class-name="mt-16 font-mono text-xs/5 font-semibold uppercase tracking-widest text-gray-500 data-[dark]:text-gray-400">Blog</h2>
+    <h1
+      class="mt-2 text-pretty text-4xl font-medium tracking-tighter text-gray-950 data-[dark]:text-white sm:text-6xl">
+      What’s happening at Radiant.
+    </h1>
+    <p class="max-w-3xl mt-6 text-2xl font-medium text-gray-500">
+      Stay informed with product updates, company news, and insights on how
+      to sell smarter at your company.
+    </p>
+  </Container>
+  <!-- {page === 1 && !category && <FeaturedPosts />} -->
+  <Container class-name="mt-16 pb-24">
+    <ContentDoc>
+      <template #not-found>
+        <NoArticle />
+      </template>
 
-  <ContentDoc>
-    <template #not-found>
-      <NoArticle />
-    </template>
-
-    <template #default="{ doc }">
-      <p class="article-date">{{ doc.date }}</p>
-      <h1 class="article-title">{{ doc.title }}</h1>
-      <ContentRenderer
-        :value="doc"
-        class="page-formating" />
-    </template>
-  </ContentDoc>
-</Container> -->
-<ContentDoc />
+      <template #default="{ doc }">
+        <ContentRenderer
+          :value="doc"
+          class="page-formating" />
+      </template>
+    </ContentDoc>
+  </Container>
+</section>
 </template>
 
 <script setup lang="ts">
-// import Container from '~/components/blog/Container.vue'
-// import NoArticle from '~/components/blog/NoArticle.vue'
-
+import Container from '~/components/blog/Container.vue'
+import NoArticle from '~/components/blog/NoArticle.vue'
+import GradientBackground from '~/components/blog/GradientBackground.vue'
 </script>
