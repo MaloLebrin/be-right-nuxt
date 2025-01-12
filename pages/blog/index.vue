ea8a42c65e04bb62de202a075b5523c609296984
MaloLebrin <malolebrin@gmail.com>
2025-01-12 22:31:24 +0100

    feat: implement blog post detail and index pages with conditional rendering

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,31 +1,37 @@
 <template>
 <section class="py-16 overflow-hidden">
   <GradientBackground />
-  <Container
-    v-if="!post">
+  <Container>
     <h2 class-name="mt-16 font-mono text-xs/5 font-semibold uppercase tracking-widest text-gray-500 data-[dark]:text-gray-400">Blog</h2>
-    <NoArticle />
-  </Container>
-  <Container v-else>
     <h1
       class="mt-2 text-pretty text-4xl font-medium tracking-tighter text-gray-950 data-[dark]:text-white sm:text-6xl">
-      {{post?.title}}
+      What’s happening at Radiant.
     </h1>
     <p class="max-w-3xl mt-6 text-2xl font-medium text-gray-500">
-      {{post?.subtitle}}
+      Stay informed with product updates, company news, and insights on how
+      to sell smarter at your company.
     </p>
+  </Container>
+  <!-- {page === 1 && !category && <FeaturedPosts />} -->
+  <Container
+    v-if="data && data.length > 0"
+    class-name="mt-16 pb-24">
+    <BlogArticleListCards :posts="data" />
+  </Container>
+  <Container
+    v-else
+    class-name="mt-16 pb-24">
+    <NoArticle />
   </Container>
 </section>
 </template>
-
+  
 <script setup lang="ts">
 import Container from '~/components/blog/Container.vue'
 import NoArticle from '~/components/blog/NoArticle.vue'
 import GradientBackground from '~/components/blog/GradientBackground.vue'
-import type { Post } from '~/types'
-
-const route = useRoute()
-const slug = route.params.slug ? route.params.slug[0] : ''
-
-const { data: post } = await useAsyncData<Post>('', async () => await queryContent('/blog').where({ slug: slug }).findOne() as unknown as Post)
+import type { Post } from '~/types';
+  
+const { data } = await useAsyncData<Post[]>('', async () => await queryContent('/blog').find() as unknown as Post[])
 </script>
+  
