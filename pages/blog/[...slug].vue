273c7820dad3eaaa409d41ce197f19f3c74f6ea6
MaloLebrin <malolebrin@gmail.com>
2025-01-12 22:47:54 +0100

    🔍 add SubHeading component and integrate published date in blog post view

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -7,6 +7,11 @@
     <NoArticle />
   </Container>
   <Container v-else>
+    <Subheading
+      v-if="post.publishedAt"
+      additionnal-classes="mt-16">
+      {{$toFormat(post.publishedAt, 'dddd, MMMM D, YYYY')}}
+    </Subheading>
     <h1
       class="mt-2 text-pretty text-4xl font-medium tracking-tighter text-gray-950 data-[dark]:text-white sm:text-6xl">
       {{post?.title}}
@@ -22,10 +27,24 @@
 import Container from '~/components/blog/Container.vue'
 import NoArticle from '~/components/blog/NoArticle.vue'
 import GradientBackground from '~/components/blog/GradientBackground.vue'
+import Subheading from '~/components/blog/article/SubHeading.vue'
 import type { Post } from '~/types'
 
 const route = useRoute()
 const slug = route.params.slug ? route.params.slug[0] : ''
 
 const { data: post } = await useAsyncData<Post>('', async () => await queryContent('/blog').where({ slug: slug }).findOne() as unknown as Post)
+
+useSeoMeta({
+  title: post.value?.title,
+  description: post.value?.shortDescription,
+  ogImage: post.value?.imageUrl,
+  ogImageAlt: `Image de couverture de l'article ${post.value?.title}`,
+  author: 'Malo Lebrin',
+  articleModifiedTime: post.value?.publishedAt,
+  articlePublishedTime: post.value?.publishedAt,
+  articleTag: [] // FIXME: Add tags
+  // ogUrl: `https://www.alexandrerenaud.ca/blog/${slug}`, FIXME Not working 
+  // url: `https://www.alexandrerenaud.ca/blog/${slug}` // FIXME: Not working
+})
 </script>
