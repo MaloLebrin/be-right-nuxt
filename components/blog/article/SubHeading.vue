f25a79200c40fa1fd087eab50289a9cc6c493b1f
MaloLebrin <malolebrin@gmail.com>
2025-01-12 22:47:54 +0100

    🔍 add SubHeading component and integrate published date in blog post view

---
+++ components/blog/article/SubHeading.vue
@@ -0,0 +1,13 @@
+<template>
+<p
+  :class="[
+    'font-mono font-semibold tracking-widest text-gray-500 uppercase text-xs/5',
+    additionnalClasses,
+  ]"><slot/></p>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  additionnalClasses?: string
+}>()
+</script>
