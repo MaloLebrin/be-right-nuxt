6c486428b4be7172e83318209b9f9fc2e74536ae
MaloLebrin <malolebrin@gmail.com>
2025-01-14 10:02:36 +0100

    ✨ update category titles for improved clarity and relevance in blog content

--- utils/blog.ts
+++ utils/blog.ts
@@ -2,31 +2,31 @@ import type { Category, CategorySlug } from "~/types/Blog";
 
 export const CategoryMap = new Map<CategorySlug, Category>([
   ['transformation-digitale-droits-image', {
-    title: 'Innovation Technologique et Droits Numériques',
+    title: 'Innovation Technologique',
     slug: 'transformation-digitale-droits-image',
     description: "Analyse des technologies émergentes qui révolutionnent la gestion, la protection et la monétisation des droits à l'image."
   }],
 
   ['droits-image-juridique', {
-    title: "Droits à l'Image : Enjeux Juridiques et Stratégiques",
+    title: "Enjeux Juridiques et Stratégiques",
     slug: 'droits-image-juridique',
     description: "Exploration approfondie des aspects légaux, réglementaires et stratégiques liés à la gestion des droits à l'image dans un environnement numérique complexe."
   }],
 
   ["securite-conformite-droits-image", {
-    title: "Sécurisation et Conformité des Actifs Visuels",
+    title: "Sécurisation et Conformité",
     slug: "securite-conformite-droits-image",
     description: "Stratégies et outils pour protéger les contenus visuels, garantir leur authenticité et prévenir les utilisations non autorisées."
   }],
 
   ['automatisation-droits-image', {
-    title: "Optimisation et Efficacité dans la Gestion des Droits",
+    title: "Gestion des Droits à l'image",
     slug: 'automatisation-droits-image',
     description: "Méthodes et solutions pour rationaliser, automatiser et simplifier la gestion administrative des droits à l'image."
   }],
 
-  ['signature-electronique-droits-image', {
-    title: "Signature Électronique et Droits Numériques",
+  ['signature-electronique', {
+    title: "Signature Électronique",
     slug: 'signature-electronique',
     description: "Exploration des technologies de signature électronique et leur impact sur la gestion légale et sécurisée des droits à l'image."
   }],
