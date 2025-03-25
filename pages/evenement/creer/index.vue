d8d7047527498f70c78deae8c44088756c157848
MaloLebrin <malolebrin@gmail.com>
2025-03-25 08:29:19 +0100

    ✨ Update layout structure in create.vue and index.vue to use container for improved responsiveness and consistency in design

--- pages/evenement/creer/index.vue
+++ pages/evenement/creer/index.vue
@@ -1,5 +1,9 @@
 <template>
-<EventFormStep1 class="px-4 lg:px-8" />
+<PageAuthWrapper>
+  <div class="container flex px-4 py-4 mx-auto mb-8 lg:px-8 xl:max-w-[1100px]">
+    <EventFormStep1 class="px-4 lg:px-8" />
+  </div>
+</PageAuthWrapper>
 </template>
 
 <script setup lang="ts">
